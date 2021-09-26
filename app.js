ca9c00a89bc009b73c168a6462a9e340ba3ce116
Aagman Suri <aagmansuri96@gmail.com>
2021-09-26 16:57:20 +0530

    style , catch added

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ var url = "https://api.funtranslations.com/translate/ferb-latin.json";
 const getTranslatedUrl = (text) => {
   return url + "?text=" + text;
 };
-
+const errorHandel = () => {
+  alert("Server Not Woring");
+};
 const clickHandeler = () => {
   var inputText = inputarea.value;
   fetch(getTranslatedUrl(inputText))
     .then((response) => response.json())
     .then((data) => {
       outputdiv.innerHTML = data.contents.translated;
-    });
+    })
+    .catch(errorHandel);
 };
 
 clickMeBtn.addEventListener("click", clickHandeler);
