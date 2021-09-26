9249ea08f98b187b585ae667850c3dbb35c6b7da
Aagman Suri <aagmansuri96@gmail.com>
2021-09-26 12:54:30 +0530

    fetch call , output div print

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello script is working");
+var clickMeBtn = document.querySelector("#btn");
+var inputarea = document.querySelector("#inputid");
+var outputdiv = document.querySelector("#upload");
+
+var url = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+const getTranslatedUrl = (text) => {
+  return url + "?text=" + text;
+};
+
+const clickHandeler = () => {
+  var inputText = inputarea.value;
+  fetch(getTranslatedUrl(inputText))
+    .then((response) => response.json())
+    .then((data) => {
+      outputdiv.innerHTML = data.contents.translated;
+    });
+};
+
+clickMeBtn.addEventListener("click", clickHandeler);
